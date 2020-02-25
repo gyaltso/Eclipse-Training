b0ab7968239472b43e1b2107a4c320c960f7ce62
Gyaltso <neerajbhusare@gyaltso.com>
2020-02-26 01:16:34 +0530

    Committing empty projects that will later contain the code for View, Editors, and Wizards
    Signed-off-by: Gyaltso <neerajbhusare@gyaltso.com>

---
+++ com.gyaltso.training/com.gyaltso.training.rcp/com.gyaltso.training.rcp.jface/com.gyaltso.training.rcp.jface.views/src/com/gyaltso/training/rcp/jface/views/EmptyView.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (C) 2020 Gyaltso Technologies (https://gyaltso.com)
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+/**
+ * 
+ */
+package com.gyaltso.training.rcp.jface.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+/**
+ * @author Neeraj Bhusare
+ *
+ */
+public class EmptyView extends ViewPart {
+
+	public EmptyView() {
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+
+	}
+
+	@Override
+	public void setFocus() {
+
+	}
+
+}
